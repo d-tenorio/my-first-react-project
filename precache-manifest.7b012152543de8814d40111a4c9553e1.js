a9bce33b9ed0cb4f576e36564f2458f254ef5f76
David Tenorio <dtenorio.work@gmail.com>
2020-03-25 19:25:41 -0600

    Updates

---
+++ precache-manifest.7b012152543de8814d40111a4c9553e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076f7c35a1c0f2285be3e744642d8818",
+    "url": "/my-first-react-project/index.html"
+  },
+  {
+    "revision": "e28b91c94bbb0b9d58d0",
+    "url": "/my-first-react-project/static/css/main.ddccaf25.chunk.css"
+  },
+  {
+    "revision": "9ced2a2446b1ec7f6086",
+    "url": "/my-first-react-project/static/js/2.db7e89aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-project/static/js/2.db7e89aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28b91c94bbb0b9d58d0",
+    "url": "/my-first-react-project/static/js/main.838aa206.chunk.js"
+  },
+  {
+    "revision": "ed476ca4f2f423716a2f",
+    "url": "/my-first-react-project/static/js/runtime-main.e943c320.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
