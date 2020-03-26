853ebde49342a3be9f8c7a524c789a87cb4f0d29
David Tenorio <dtenorio.work@gmail.com>
2020-03-25 18:55:42 -0600

    added production build files

---
+++ build/precache-manifest.f6f23da1069bf9074c8d68a6277e2652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08194483b244eaaafe225c17a5b7d332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87107742ebecbf513db6",
+    "url": "/static/css/main.ddccaf25.chunk.css"
+  },
+  {
+    "revision": "9ced2a2446b1ec7f6086",
+    "url": "/static/js/2.db7e89aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db7e89aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87107742ebecbf513db6",
+    "url": "/static/js/main.378e8c84.chunk.js"
+  },
+  {
+    "revision": "491594f1d8a0a641597e",
+    "url": "/static/js/runtime-main.766b6f6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
